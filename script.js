513e22653bcdf5f18f8899396e960cfa52fe48d4
Snubis6 <snubis9@gmail.com>
2022-04-02 03:13:29 +0200

    z

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("VM").onclick = function () {
+    location.href = "./valent.html";
+    
+};
+
+document.getElementById("ES").onclick = function () {
+    location.href = "./eva.html";
+};
+
+document.getElementById("DI").onclick = function () {
+    location.href = "./daniel.html";
+};
+
+document.getElementById("NM").onclick = function () {
+    location.href = "./niya.html";
+};
+
+document.getElementById("SK").onclick = function () {
+    location.href = "./szilard.html";
+};
+function onButtonClick(){
+
+}
